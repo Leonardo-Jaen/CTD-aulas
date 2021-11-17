0cd2b9d573ba4dbfc44d14427d84bb393077eb54
Leonardo-Jaen <Leco.jaen1@gmail.com>
2021-11-17 20:34:51 -0300

    Fiz uma tabuada

--- atividade5.js
+++ atividade5.js
@@ -1,2 +1,38 @@
-let notas = [9,8,6.5,7.5,10];
-console.log(notas[notas.length - 2]);
+let numeros = [1,5,6,4,7,8,1000,2000,3000];
+
+console.log("Tamanho do array:" + numeros.length)
+for(let i = 0;i <numeros.length;i++) {
+    console.log("Conteudo do Indice:" + i + "=" + numeros[i]);
+}
+
+let nomes = [];
+for(let i = 0; i<100;i++) {
+    nomes.push("nome" + i );
+}
+
+console.log(nomes)
+
+let html = "<ol>"
+
+for(let i = 0;i<nomes.length;i++) {
+    html += "<li>" + nomes[i] + "</li>"
+}
+
+html += "</ol>";
+
+console.log(html);
+
+let qtdPares = 0;
+let qtdImpares = 0;
+
+for(let i = 1; i <= 100;i++) {
+    if(i % 2 == 0 ) {
+        console.log("O número: " + i + " é par");
+    } else {
+        console.log("O número: " + i + " é impar")
+    }
+}
+
+console.log("quantidade de pares: " + qtdPares);
+
+console.log("quantidade de impares: " + qtdImpares);
---
+++ tabela.js
@@ -0,0 +1,6 @@
+for (let i = 1;i <= 10;i++){
+    console.log("Tabuada do " + i)
+    for (let y = 0; y <= 10;y++){
+        console.log(i + "x" + y + "=" + i*y)
+    }
+}
