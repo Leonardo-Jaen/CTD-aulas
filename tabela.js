2b8219a15ec77777fa034622715146493234138e
Leonardo-Jaen <Leco.jaen1@gmail.com>
2021-11-17 20:46:40 -0300

    outra tabuada

--- tabela.js
+++ tabela.js
@@ -1,6 +1,13 @@
-for (let i = 1;i <= 10;i++){
-    console.log("Tabuada do " + i)
-    for (let y = 0; y <= 10;y++){
-        console.log(i + "x" + y + "=" + i*y)
+//for (let i = 1;i <= 10;i++){
+//    console.log("Tabuada do " + i)
+//    for (let y = 0; y <= 10;y++){
+ //       console.log(i + "x" + y + "=" + i*y)
+  //  }
+//}
+
+for(let i = 1;i <=9;i++) {
+    for(let j = 1;j <=9;j++) {
+      console.log(i + "x" + j + " = " + i*j);  
     }
+    console.log('---------------------------')
 }
